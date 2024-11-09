8dcd29e8e0d8d9584c9d1330038ec9fcb1e946fd
ZerozAndOnez <myas.dev@gmail.com>
2024-11-09 14:33:22 +0300

    [feature] Add environment configuration and Syncfusion library

---
+++ packages/frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  production: false,
+  syncfusionLicenseKey: 'Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpTHxbf1x0ZFBMYl1bR3VPMyBoS35RckRiWXZedXZURWRUVENw'
+};
--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLicense } from '@syncfusion/ej2-base';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+registerLicense(environment.syncfusionLicenseKey);
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
