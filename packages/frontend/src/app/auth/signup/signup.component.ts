b94ce5b032f9ec658f23881ebe59567b19868e5b
ZerozAndOnez <myas.dev@gmail.com>
2024-11-15 18:40:21 +0300

    [feature] Implement sign up functionality with validation, accessibility and password rules feedback

--- packages/frontend/src/app/auth/signup/signup.component.ts
+++ packages/frontend/src/app/auth/signup/signup.component.ts
@@ -1,22 +1,68 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AuthStore } from '../store/auth.store';
-import { SyncFusionModule } from '../../modules/syncfusion.module';
+import {
+  emailValidator,
+  passwordValidator,
+} from '../../utils/validators/form-validators';
 import { AngularCommonModule } from '../../modules/angular-common.module';
+import { PennifyAppModule } from '../../modules/pennify-app.module';
+import { AuthBackgroundComponent } from '../common/auth-background/auth-background.component';
+import { AuthCardComponent } from '../common/auth-card/auth-card.component';
+import { AuthIllustrationComponent } from '../common/auth-illustration/auth-illustration.component';
+import { EmailComponent } from '../../common/components/input/email/email.component';
+import { PasswordComponent } from '../../common/components/input/password/password.component';
 
 @Component({
   standalone: true,
   selector: 'app-auth-signup',
   templateUrl: './signup.component.html',
   providers: [AuthStore],
-  imports: [SyncFusionModule, AngularCommonModule],
+  imports: [
+    AngularCommonModule,
+    RouterModule,
+    PennifyAppModule,
+    AuthBackgroundComponent,
+    AuthCardComponent,
+    AuthIllustrationComponent,
+    EmailComponent,
+    PasswordComponent,
+  ],
 })
-export class SignupComponent {
-  email = '';
-  password = '';
+export class SignupComponent implements OnInit, AfterViewInit {
+  @ViewChild(EmailComponent) emailComponent!: EmailComponent;
+  signupForm!: FormGroup;
+  focusedField: string | null = null;
 
-  constructor(public authStore: AuthStore) {}
+  constructor(private fb: FormBuilder, public authStore: AuthStore) {}
 
-  onSignup() {
-    this.authStore.signup({ email: this.email, password: this.password });
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, emailValidator]],
+      password: ['', [Validators.required, passwordValidator]],
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.emailComponent.emailInput.nativeElement.focus();
+  }
+
+  onSignup(): void {
+    if (this.signupForm.valid) {
+      this.authStore.signup(this.signupForm.value);
+    } else {
+      this.signupForm.markAllAsTouched();
+    }
+  }
+
+  onFocus(fieldName: string) {
+    this.focusedField = fieldName;
+  }
+
+  onBlur(fieldName: string) {
+    if (this.focusedField === fieldName) {
+      this.focusedField = null;
+    }
   }
 }
