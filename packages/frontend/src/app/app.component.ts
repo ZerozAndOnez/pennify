28fb475eb4a71adc95a95c9df1e7c8c9e562909d
ZerozAndOnez <myas.dev@gmail.com>
2024-11-15 16:36:50 +0300

    [cleanup] Remove boilerplate component from app component

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { SyncFusionModule } from './modules/syncfusion.module';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, SyncFusionModule],
+  imports: [RouterModule, SyncFusionModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
