57dec38e39bafb480f1ad28f84f7d39bdcb3e89a
ZerozAndOnez <myas.dev@gmail.com>
2024-11-16 23:02:58 +0300

    [setup] Enhance HTTP service to include withCredentials for secure API requests

--- packages/frontend/src/app/services/http.service.ts
+++ packages/frontend/src/app/services/http.service.ts
@@ -9,22 +9,31 @@ import { environment } from '../../environments/environment';
 })
 export class HttpService {
   private apiUrl = environment.apiUrl;
+  private config = { withCredentials: true };
 
   constructor(private http: HttpClient) {}
 
   get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(`${this.apiUrl}/${endpoint}`);
+    return this.http.get<T>(`${this.apiUrl}/${endpoint}`, this.config);
   }
 
   post<T, ResponseType>(endpoint: string, data: T): Observable<ResponseType> {
-    return this.http.post<ResponseType>(`${this.apiUrl}/${endpoint}`, data);
+    return this.http.post<ResponseType>(
+      `${this.apiUrl}/${endpoint}`,
+      data,
+      this.config
+    );
   }
 
   put<T, ResponseType>(endpoint: string, data: T): Observable<ResponseType> {
-    return this.http.put<ResponseType>(`${this.apiUrl}/${endpoint}`, data);
+    return this.http.put<ResponseType>(
+      `${this.apiUrl}/${endpoint}`,
+      data,
+      this.config
+    );
   }
 
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.apiUrl}/${endpoint}`);
+    return this.http.delete<T>(`${this.apiUrl}/${endpoint}`, this.config);
   }
 }
